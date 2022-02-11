36dcbf559f0e8328db3714a619632a55d50d470f
admiralnlson <admiralnlson@gmail.com>
2022-02-11 21:02:52 +0100

    settings to request Fortune and Sorcery decks & discard piles

--- module/castle-falkenstein.mjs
+++ module/castle-falkenstein.mjs
@@ -8,49 +8,263 @@ import { CastleFalkensteinPossessionSheet } from "./sheets/item-possession-sheet
 import { CastleFalkensteinSpellSheet } from "./sheets/item-spell-sheet.mjs";
 import { CastleFalkensteinWeaponSheet } from "./sheets/item-weapon-sheet.mjs";
 // Import helper/utility classes and constants.
-import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
 import { CASTLE_FALKENSTEIN } from "./helpers/config.mjs";
+import CastleFalkensteinSettings from "./helpers/settings.mjs";
+
+export default class CastleFalkenstein {
+
+  static get name() { return "castle-falkenstein"; }
+
+  // Convenience proxy getter for CastleFalkenstein settings.
+  static settings = new Proxy({}, {
+    get: function (target, key) {
+      try { return game.settings.get(CastleFalkenstein.name, key); }
+      catch (err) {
+        console.warn(err);
+        return undefined;
+      };
+    }
+  });
+
+  static get settingDefinitions() {
+    const deckSetting = {
+      type: String, default: "",
+      getChoices: () => ({
+        "": "",
+        ...Object.fromEntries(
+          game.cards
+            .filter(stack => stack.type === "deck")
+            .map(stack => [stack.id, stack.name])
+        )
+      })
+    };
+
+    const pileSetting = {
+      type: String, default: "",
+      getChoices: () => ({
+        "": "",
+        ...Object.fromEntries(
+          game.cards
+            .filter(stack => stack.type === "pile")
+            .map(stack => [stack.id, stack.name])
+        )
+      })
+    };
+
+    return {
+      fortuneDeck: deckSetting,
+      fortuneDiscardPile: pileSetting,
+      sorceryDeck: deckSetting,
+      sorceryDiscardPile: pileSetting
+    };
+  }
+
+  static get fortuneDeck() {
+    return game.cards.get(this.settings.fortuneDeck);
+  }
+  static get sorceryDeck() {
+    return game.cards.get(this.settings.sorceryDeck);
+  }
+  static get fortuneDiscardPile() {
+    return game.cards.get(this.settings.fortuneDiscardPile);
+  }
+  static get sorceryDiscardPile() {
+    return game.cards.get(this.settings.sorceryDiscardPile);
+  }
+
+  /**
+   * Re-renders all CastleFalkenstein sheets to allow settings to take effect.
+   *
+   * @static
+   * @memberof CastleFalkenstein
+   */
+  static refreshSheets() {
+    Object.values(ui.windows)
+      .filter(window => window.isCastleFalkenstein)
+      .forEach(window => window.render(false, { updateClasses: true }));
+  }
+
+  /**
+   * Refreshes CastleFalkenstein sheets for all players.
+   *
+   * @static
+   * @memberof CastleFalkenstein
+   */
+  static async refreshSheetsAll() {
+    if (game.user.isGM) await game.socket.emit(this.socketName, {
+      command: "refreshSheets"
+    });
+
+    this.refreshSheets();
+  }
+
+  /**
+   * Display the sheet for the card specified in the data
+   *
+   * @static
+   * @param {{ pile: string, card: string }} data - Ids of the card and its container
+   * @memberof CastleFalkenstein
+   */
+  static async showCard(data) {
+    game.cards.get(data.pile)?.cards?.get(data.card)?.sheet?.render(true)
+  }
+
+  static onInit() {
+    this.registerSettings();
+    this.registerSheets();
+
+    // Add custom constants for configuration.
+    CONFIG.CASTLE_FALKENSTEIN = CASTLE_FALKENSTEIN;
+
+    // Define custom Document classes
+    CONFIG.Actor.documentClass = CastleFalkensteinActor;
+    CONFIG.Item.documentClass = CastleFalkensteinItem;
+  }
+
+  static async onReady() {
+    await this.preLoadTemplates();
+
+    Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
+
+    // Event listener to close all context menus when the user clicks outside of them.
+    document.addEventListener("click", event => {
+      document.querySelectorAll(".castle-falkenstein .card").forEach(card => card.classList.remove("show-ctx"));
+    });
+
+    game.socket.on(this.socketName, this._onSocketMessage.bind(this));
+
+    console.log(game.i18n.localize("castle-falkenstein.console.log.ready"));
+  }
+
+  static registerSettings() {
+    Object.entries(this.settingDefinitions).forEach(([key, def]) => {
+      game.settings.register(CastleFalkenstein.name, key, {
+        ...def,
+        scope: def.scope ?? "world",
+        config: false,
+        name: `castle-falkenstein.settings.${key}.name`,
+        hint: `castle-falkenstein.settings.${key}.hint`
+      });
+    })
+
+    game.settings.registerMenu(this.name, 'settingsMenu', {
+      name: game.i18n.localize("castle-falkenstein.settings.name"),
+      icon: "fas fa-bars",
+      label: game.i18n.localize("castle-falkenstein.settings.label"),
+      type: CastleFalkensteinSettings,
+    });
+  }
+
+  static registerSheets() {
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("castle-falkenstein", CastleFalkensteinActorSheet, { makeDefault: true });
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("castle-falkenstein", CastleFalkensteinAbilitySheet, { types: ["ability"], makeDefault: true });
+    Items.registerSheet("castle-falkenstein", CastleFalkensteinPossessionSheet, { types: ["possession"], makeDefault: true });
+    Items.registerSheet("castle-falkenstein", CastleFalkensteinSpellSheet, { types: ["spell"], makeDefault: true });
+    Items.registerSheet("castle-falkenstein", CastleFalkensteinWeaponSheet, { types: ["weapon"], makeDefault: true });
+  }
+
+  // Load all the templates for handlebars partials.
+  static async preLoadTemplates() {
+    return loadTemplates([
+      // Actor partials
+      "systems/castle-falkenstein/templates/parts/actor-abilities.hbs",
+      "systems/castle-falkenstein/templates/parts/actor-possessions.hbs",
+      "systems/castle-falkenstein/templates/parts/actor-spells.hbs",
+      "systems/castle-falkenstein/templates/parts/actor-weapons.hbs"
+    ]);
+  }
+
+  /**
+   * Create a Macro from an Item drop.
+   * Get an existing item macro if one exists, otherwise create a new one.
+   * @param {Object} data     The dropped data
+   * @param {number} slot     The hotbar slot to use
+   * @returns {Promise}
+   */
+  static async createItemMacro(data, slot) {
+    if (data.type !== "Item") return;
+    if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
+    const item = data.data;
+
+    // Create the macro command
+    const command = `game.CastleFalkenstein.rollItemMacro("${item.name}");`;
+    let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
+    if (!macro) {
+      macro = await Macro.create({
+        name: item.name,
+        type: "script",
+        img: item.img,
+        command: command,
+        flags: { "CastleFalkenstein.itemMacro": true }
+      });
+    }
+    game.user.assignHotbarMacro(macro, slot);
+    return false;
+  }
+  
+  /**
+   * Create a Macro from an Item drop.
+   * Get an existing item macro if one exists, otherwise create a new one.
+   * @param {string} itemName
+   * @return {Promise}
+   */
+  static rollItemMacro(itemName) {
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    const item = actor ? actor.items.find(i => i.name === itemName) : null;
+    if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+
+    // Trigger the item roll
+    return item.roll();
+  }
+
+  /**
+   * The name of the web socket for this module
+   *
+   * @static
+   * @memberof CastleFalkenstein
+   */
+  static get socketName() {
+    return `module.${this.name}`;
+  }
+
+  /**
+   * Handles socket messages.
+   *
+   * Delegates the message to the appropriate handler.
+   *
+   * @static
+   * @param {Object} message
+   * @param {String} message.command - The command to handle.
+   * @param {Object} message.data    - The data for the command.
+   * @memberof CastleFalkenstein
+   */
+  static async _onSocketMessage({ command, data }) {
+    if (!this[command]) return;
+    await this[command](data);
+  }
+}
 
 /* -------------------------------------------- */
-/*  Init Hook                                   */
+/*  Declare Hookslpers                          */
 /* -------------------------------------------- */
 
-Hooks.once('init', async function() {
+Hooks.on("init", CastleFalkenstein.onInit.bind(CastleFalkenstein));
 
-  // Add utility classes to the global game object so that they're more easily
-  // accessible in global contexts.
-  game.CastleFalkenstein = {
-    CastleFalkensteinActor,
-    CastleFalkensteinItem,
-    rollItemMacro
-  };
+Hooks.on("ready", CastleFalkenstein.onReady.bind(CastleFalkenstein));
 
-  // Add custom constants for configuration.
-  CONFIG.CASTLE_FALKENSTEIN = CASTLE_FALKENSTEIN;
-
-  // Define custom Document classes
-  CONFIG.Actor.documentClass = CastleFalkensteinActor;
-  CONFIG.Item.documentClass = CastleFalkensteinItem;
-
-  // Register sheet application classes
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("castle-falkenstein", CastleFalkensteinActorSheet, { makeDefault: true });
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("castle-falkenstein", CastleFalkensteinAbilitySheet, { types: ["ability"], makeDefault: true });
-  Items.registerSheet("castle-falkenstein", CastleFalkensteinPossessionSheet, { types: ["possession"], makeDefault: true });
-  Items.registerSheet("castle-falkenstein", CastleFalkensteinSpellSheet, { types: ["spell"], makeDefault: true });
-  Items.registerSheet("castle-falkenstein", CastleFalkensteinWeaponSheet, { types: ["weapon"], makeDefault: true });
-
-  // Preload Handlebars templates.
-  return preloadHandlebarsTemplates();
-});
+window.CastleFalkenstein = CastleFalkenstein;
 
 /* -------------------------------------------- */
 /*  Handlebars Helpers                          */
 /* -------------------------------------------- */
 
 // If you need to add Handlebars helpers, here are a few useful examples:
-Handlebars.registerHelper('concat', function() {
+Handlebars.registerHelper('concat', function () {
   var outStr = '';
   for (var arg in arguments) {
     if (typeof arguments[arg] != 'object') {
@@ -60,65 +274,6 @@ Handlebars.registerHelper('concat', function() {
   return outStr;
 });
 
-Handlebars.registerHelper('toLowerCase', function(str) {
+Handlebars.registerHelper('toLowerCase', function (str) {
   return str.toLowerCase();
 });
-
-/* -------------------------------------------- */
-/*  Ready Hook                                  */
-/* -------------------------------------------- */
-
-Hooks.once("ready", async function() {
-  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-  Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
-});
-
-/* -------------------------------------------- */
-/*  Hotbar Macros                               */
-/* -------------------------------------------- */
-
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {Object} data     The dropped data
- * @param {number} slot     The hotbar slot to use
- * @returns {Promise}
- */
-async function createItemMacro(data, slot) {
-  if (data.type !== "Item") return;
-  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
-  const item = data.data;
-
-  // Create the macro command
-  const command = `game.CastleFalkenstein.rollItemMacro("${item.name}");`;
-  let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
-  if (!macro) {
-    macro = await Macro.create({
-      name: item.name,
-      type: "script",
-      img: item.img,
-      command: command,
-      flags: { "CastleFalkenstein.itemMacro": true }
-    });
-  }
-  game.user.assignHotbarMacro(macro, slot);
-  return false;
-}
-
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemName
- * @return {Promise}
- */
-function rollItemMacro(itemName) {
-  const speaker = ChatMessage.getSpeaker();
-  let actor;
-  if (speaker.token) actor = game.actors.tokens[speaker.token];
-  if (!actor) actor = game.actors.get(speaker.actor);
-  const item = actor ? actor.items.find(i => i.name === itemName) : null;
-  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
-
-  // Trigger the item roll
-  return item.roll();
-}
