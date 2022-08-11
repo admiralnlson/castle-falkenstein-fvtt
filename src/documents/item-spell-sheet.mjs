5278d7a9911d88b0b5d35f9673c8716fe97e7bbd
admiralnlson <admiralnlson@gmail.com>
2022-08-11 02:15:07 +0200

    system name property replaced with id

--- src/documents/item-spell-sheet.mjs
+++ src/documents/item-spell-sheet.mjs
@@ -12,7 +12,7 @@ export class CastleFalkensteinSpellSheet extends ItemSheet {
    */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: [CastleFalkenstein.name, "sheet", "item"],
+      classes: [CastleFalkenstein.id, "sheet", "item"],
       template: "systems/castle-falkenstein/src/documents/item-spell-sheet.hbs",
       width: 460,
       height: 300,
