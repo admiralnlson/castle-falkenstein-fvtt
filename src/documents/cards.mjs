3db7bedad4c2bb2fd2c4450a35da1ab648e2d7bf
admiralnlson <admiralnlson@gmail.com>
2022-08-11 02:14:45 +0200

    spellBeingCast rework

--- src/documents/cards.mjs
+++ src/documents/cards.mjs
@@ -8,13 +8,36 @@ export class CastleFalkensteinCards extends Cards {
   async _resetStack({updateData={}, chatNotification=true}={}) {
     // Let's short-circuit the default behaviour of cards returning from CF hands to decks by default in Core, and discard them to the adequate pile instead
     if (this.type === "hand") {
-      const handType = this.getFlag(CastleFalkenstein.name, "type");
+      const handType = this.getFlag(CastleFalkenstein.id, "type");
 
-      await this.pass(CastleFalkenstein.discardPile(handType), this.cards.map(card => card.id), {chatNotification: false});
+      await this.pass(CastleFalkenstein.discardPile(handType), this.cards.map(card => card.id), {chatNotification: chatNotification});
     }
     else {
       super._resetStack({updateData, chatNotification});
     }
+  }
+
+  // "spellBeingCast" flag object structure = {
+  //    actorItemId: <id of the item within the actor>,
+  //    definitions: {
+  //      <enum of properties is documented in config.mjs>
+  //    }
+  //  }
+
+  get spellBeingCast() {
+    if (this.type === "hand" &&
+        this.getFlag(CastleFalkenstein.id, "type") === "sorcery") {
+      return this.getFlag(CastleFalkenstein.id, "spellBeingCast");
+    }
+  }
+
+  async defineSpell(spellBeingCast) {
+    await this.unsetFlag(CastleFalkenstein.id, 'spellBeingCast');
+    await this.setFlag(CastleFalkenstein.id, 'spellBeingCast', spellBeingCast);
+  }
+
+  async stopCasting() {
+    await this.unsetFlag(CastleFalkenstein.id, 'spellBeingCast');
   }
 
 }
