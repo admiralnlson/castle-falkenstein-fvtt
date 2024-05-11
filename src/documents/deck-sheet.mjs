82c9047100a95a7e6f481104a1fa1cbda2468fb4
admiralnlson <admiralnlson@gmail.com>
2024-05-11 02:49:37 +0200

    variations + fortune<>sorcery check + monarch support dropped

---
+++ src/documents/deck-sheet.mjs
@@ -0,0 +1,18 @@
+import { CASTLE_FALKENSTEIN } from "../config.mjs";
+import { CastleFalkenstein } from "../castle-falkenstein.mjs";
+
+/**
+ * Sheet for the Cards Deck.
+ * @extends {CardsConfig}
+ */
+export class CastleFalkensteinDeckSheet extends CardsConfig {
+
+  /** @override */
+  async _onDrop(event) {
+    if (! await CastleFalkenstein.onDropOnCardStack(event, this))
+      return;
+
+    return super._onDrop(event);
+  }
+
+}
